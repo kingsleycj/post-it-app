2ec83749680d245c2aa3788578886304fc3e56ac
kingsleycj <primekings.kc@gmail.com>
2023-03-10 03:46:54 +0100

    Feat: Created comment routes

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const postController = require("../controllers/post.controller");
 const commentController = require("../controllers/comment.controller");
 
 // Create a new comment on a post
-router.post("/:postId/comments", commentController.createComment);
+router.post("/users/:userId/posts/:postId/comments", commentController.createComment);
 
-// // Get all comments on a post
-// router.get("/:postId/comments", postController.getCommentsByPostId);
+// Get all comments on a post
+router.get("/users/:userId/posts/:postId/comments", commentController.getAllComments);
 
 // Get a single comment by ID
-router.get("/:postId/comments/:commentId", commentController.getCommentById);
+router.get("/users/:userId/posts/:postId/comments/:commentId", commentController.getCommentById);
 
 // Update a single comment by ID
-router.patch("/:postId/comments/:commentId", commentController.updateCommentById);
+router.put("/users/:userId/posts/:postId/comments/:commentId", commentController.updateCommentById);
 
 // Delete a single comment by ID
-router.delete("/comments/:id", commentController.deleteCommentById);
+router.delete("/users/:userId/posts/:postId/comments/:commentId", commentController.deleteCommentById);
 
 module.exports = router;
--- routes/post.route.js
+++ routes/post.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post.controller");
 const commentRoute = require("./comment.route");
-const postRoute = require("./post.route");
 
 router.use(commentRoute)
 
